469299f5f9f2d92d9daae80b360fc9b3e40fe40b
Lawal Abdullah Folarin <sirojudeenopeyemi@gmail.com>
2025-10-19 22:29:35 +0100

    update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -2,7 +2,8 @@
 -- The database name (alx_book_store) is assumed to be passed as an argument
 -- to the mysql command when executing this script.
 
-INSERT INTO customer (customer_id, customer_name, email, address) VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'), -- Corrected spacing here
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
