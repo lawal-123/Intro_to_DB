2ea3ddd84ab532bb8d7b70a36365a1d200ee74fe
Lawal Abdullah Folarin <sirojudeenopeyemi@gmail.com>
2025-10-19 22:26:09 +0100

    update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -3,6 +3,6 @@
 -- to the mysql command when executing this script.
 
 INSERT INTO customer (customer_id, customer_name, email, address) VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'), -- Corrected spacing here
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
 (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
